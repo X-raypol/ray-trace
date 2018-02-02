fcf2e4b00c0354eb3d236dc98d3c6a344a0b93eb
hamogu <hgunther@mit.edu>
2018-02-02 17:03:22 -0500

    Turn Redsox into classes wiht conf dict for easier manipulation

--- redsox/gratings.py
+++ redsox/gratings.py
@@ -1,24 +1,30 @@
 import numpy as np
 from scipy import optimize
-
+import astropy.units as u
 from marxs.optics import OpticalElement
 from marxs.simulator import ParallelCalculated
+from arcus.ralfgrating import CATGratingwithL1, globalorderselector
+from transforms3d.axangles import axangle2mat
+
+
+def blazemat(blazeang):
+    return axangle2mat(np.array([0, 0, 1]), blazeang.to(u.rad).value)
 
 
 class GratingGrid(ParallelCalculated, OpticalElement):
     id_col = 'facet'
-    dg = 0.0002  # in mm
     G = 8.8e-8   # 0.88 Ang / mm
-    beta_lim = np.deg2rad([3.5, 5.05])
     d_frame = 0.5
+    elem_class = CATGratingwithL1
+    order_selector = globalorderselector
 
     def beta_from_betamax(self, betamax):
-        '''return beta of granting center given beta on the rightmost border.
+        '''return beta of grating center given beta on the rightmost border.
         This assumes gratings are oriented such that the central ray is the normal.
         '''
         def bm(beta):
             rg = self.elem_rg(0., beta)
-            return np.abs(beta) + np.arcsin(self.zoom_grating[1] / rg) - np.abs(betamax)
+            return np.abs(beta) + np.arcsin(self.conf['gratingzoom'][1] / rg) - np.abs(betamax)
         return optimize.root(bm, betamax)['x'][0]
 
     def ymax_from_beta(self, beta):
@@ -27,7 +33,7 @@ class GratingGrid(ParallelCalculated, OpticalElement):
 
     def distribute_on_fixed_beta(self, beta):
         l = 2 * np.abs(self.ymax_from_beta(beta))
-        n = np.round(l / (2 * self.zoom_grating[0] + self.d_frame))
+        n = np.round(l / (2 * self.conf['gratingzoom'][0] + self.d_frame))
         # y = (np.arange(n) - n/2) * (2 * self.zoom_grating[0] + self.d_frame)
         # unfinished because I realized it's not the best way to do it
         # but I keep it here as reference for later
@@ -36,33 +42,43 @@ class GratingGrid(ParallelCalculated, OpticalElement):
         ''' Technically, rg depends on gamma and d_gamma depends on rg and beta
         so this is an iterative problem, but this approximation is good enough for now.
         '''
-        d_gamma = (2 * self.zoom_grating[0] + self.d_frame) / rg
+        d_gamma = (2 * self.conf['gratingzoom'][0] + self.d_frame) / rg
         return (np.arange(n) - n / 2) * d_gamma
 
     def distribute_betas(self):
         beta = []
-        betalow = self.beta_lim[1]
-        while betalow > self.beta_lim[0]:
+        betalow = self.conf['beta_lim'][1]
+        while betalow > self.conf['beta_lim'][0]:
             beta.append(self.beta_from_betamax(betalow))
-            betalow = beta[-1] - np.arcsin((self.zoom_grating[1] + .5) / self.elem_rg(0., beta[-1]))
+            betalow = beta[-1] - np.arcsin((self.conf['gratingzoom'][1] + .5) / self.elem_rg(0., beta[-1]))
 
-        betalow = -self.beta_lim[1]
-        while betalow < -self.beta_lim[0]:
+        betalow = -self.conf['beta_lim'][1]
+        while betalow < -self.conf['beta_lim'][0]:
             beta.append(self.beta_from_betamax(betalow))
-            betalow = beta[-1] + np.arcsin((self.zoom_grating[1] + .5) / self.elem_rg(0., beta[-1]))
+            betalow = beta[-1] + np.arcsin((self.conf['gratingzoom'][1] + .5) / self.elem_rg(0., beta[-1]))
         return beta
 
-    def __init__(self, **kwargs):
+    def __init__(self, channel, conf, **kwargs):
         kwargs['pos_spec'] = self.elempos
         if 'normal_spec' not in kwargs.keys():
             kwargs['normal_spec'] = np.array([0., 0., 0., 1.])
         if 'parallel_spec' not in kwargs.keys():
             kwargs['parallel_spec'] = np.array([0., 1., 0., 0.])
-        self.zoom_grating = kwargs.pop('zoom_grating')
+        self.conf = conf
+        self.channel = channel
+        kwargs['elem_class'] = self.elem_class
+        kwargs['elem_args'] = {'d': conf['d'],
+                               'order_selector': self.order_selector,
+                               'zoom': conf['gratingzoom'],
+                               'orientation': np.dot(axangle2mat([1, 0, 0], np.pi / 2),
+                                                     blazemat(conf['blazeang'])),
+                               }
         super(GratingGrid, self).__init__(**kwargs)
+        self.elem_pos = [np.dot(conf['rotchan'][channel], e) for e in self.elem_pos]
+        self.generate_elements()
 
     def elem_rg(self, gamma, beta):
-        return self.dg/(np.sqrt(2) * self.G * np.cos(gamma)**3 * (np.sin(beta) + np.cos(beta)))
+        return self.conf['d']/(np.sqrt(2) * self.G * np.cos(gamma)**3 * (np.sin(beta) + np.cos(beta)))
 
     def elem_center(self, gamma, beta):
         rg = self.elem_rg(gamma, beta)
--- redsox/redsox.py
+++ redsox/redsox.py
@@ -5,11 +5,13 @@ from scipy.interpolate import interp1d
 from transforms3d.euler import euler2mat
 import transforms3d
 from astropy.table import Table
+import astropy.units as u
 
 import marxs
 from marxs import optics, simulator
 
-from read_grating_data import InterpolateRalfTable, RalfQualityFactor
+from arcus.ralfgrating import (RalfQualityFactor,
+                               catsupport, catsupportbars)
 from gratings import GratingGrid
 from mlmirrors import LGMLMirror
 
@@ -22,172 +24,234 @@ redsoxbase = '/melkor/d1/guenther/Dropbox/REDSoX File Transfers'
 inputpath = os.path.join(redsoxbase, 'raytrace', 'inputdata')
 
 xyz2zxy = np.array([[0., 0, 1, 0], [1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 0, 1]]).T
-rotchan2 = euler2aff(np.pi * 2 / 3, 0, 0, 'szyz')
-rotchan3 = euler2aff(-np.pi * 2 / 3, 0, 0, 'szyz')
-
-# Optics
-# Scatter as FWHM ~30 arcsec. Divide by 2.3545 to get Gaussian sigma.
-# Scatter numbers are still all wrong.
-aper = optics.CircleAperture(orientation=xyz2zxy[:3, :3], position=[0, 0, 2700],
-                             zoom=[1, 223, 223], r_inner=165)
-
-# Nickel reflectivity
-ni_refl = Table.read(os.path.join(inputpath, 'ni_refl_1deg.txt'),
-                     format='ascii.no_header', data_start=2,
-                     names=['energy', 'refl'])
 
 
-mirror = optics.FlatStack(orientation=xyz2zxy[:3, :3], position=[0, 0, 2500],
-                          zoom=[1, 225, 225],
-                          elements=[optics.PerfectLens,
-                                    optics.RadialMirrorScatter,
-                                    optics.EnergyFilter,
-                                    optics.EnergyFilter
-                                ],
-                          keywords=[{'focallength': 2500},
-                                    {'inplanescatter': 30. / 2.3545 / 3600 / 180. * np.pi,
-                                     'perpplanescatter': 10 / 2.345 / 3600. / 180. * np.pi},
-                                    {'filterfunc': interp1d(ni_refl['energy'] * 1e-3, ni_refl['refl']**2),
-                                     'name': 'double Ni reflectivity'},
-                                    {'filterfunc': lambda x: np.ones_like(x) * 0.88,
-                                     'name': 'support spider'}
-                                    ])
-mirror.elements[1].display = {'color': (0.0, 0.5, 0.0), 'opacity': 0.1}
+def FWHMarcs2sigrad(x):
+    '''Convert FWHM in arcsec to Gaussian sigma in rad'''
+    return x / 2.3545 / 3600 / 180. * np.pi
+
+
+conf = {'aper_z': 2700.,
+        'aper_rin': 165.,
+        'aper_rout': 223.,
+        'mirr_rout': 225,
+        'f': 2500,
+        'inscat': FWHMarcs2sigrad(30.),
+        'perpscat': FWHMarcs2sigrad(10.),
+        'blazeang': 0.8 * u.degree,
+        'gratingzoom': [0.25, 4, 5],
+        'd': 2e-4,
+        'rotchan': {'1': np.eye(4),
+                    '2': euler2aff(np.pi * 2 / 3, 0, 0, 'szyz'),
+                    '3': euler2aff(-np.pi * 2 / 3, 0, 0, 'szyz')},
+        'grat_id_offset': {'1': 1000, '2': 2000, '3': 3000},
+        'beta_lim': np.deg2rad([3.5, 5.05]),
+        'mirrorfile': 'ml_refl_2017_withCrSc_width.txt',
+        'MLzoom': [0.25, 15., 5.],
+        'MLpos': [44.55, 0, 0],
+        'pixsize': 0.016,
+        'detsize0': [408, 1608],
+        'detsize123': [1632, 1608],
+        }
+
+
+# Aperture
+class CircleAperture(optics.CircleAperture):
+    def __init__(self, channels, conf):
+        super(CircleAperture, self).__init__(orientation=xyz2zxy[:3, :3],
+                                             position=[0, 0, conf['aper_z']],
+                                             zoom=[1, conf['aper_rout'], conf['aper_rout']],
+                                             r_inner=conf['aper_rin'])
+
+
+class SimpleMirror(optics.FlatStack):
+    # Optics
+    # Scatter as FWHM ~30 arcsec. Divide by 2.3545 to get Gaussian sigma.
+    # Scatter numbers are still all wrong.
+    spider_fraction = 0.88
+
+    def refl(self):
+        '''Read Ni reflectivity'''
+        self.refl = Table.read(os.path.join(inputpath, 'ni_refl_1deg.txt'),
+                               format='ascii.no_header', data_start=2,
+                               names=['energy', 'refl'])
+        self.refl['energy'] *= 1e-3  # convert eV to keV
+        return self.refl
+
+    def __init__(self, channels, conf):
+        refl = self.refl()
+        kwords = [{'focallength': conf['f']},
+                  {'inplanescatter': conf['inscat'],
+                   'perpplanescatter': conf['perpscat']},
+                  {'filterfunc': interp1d(refl['energy'], refl['refl']**2),
+                   'name': 'double Ni reflectivity'},
+                  {'filterfunc': lambda x: np.ones_like(x) * self.spider_fraction,
+                   'name': 'support spider'}]
+        super(SimpleMirror, self).__init__(orientation=xyz2zxy[:3, :3],
+                                           position=[0, 0, conf['f']],
+                                           zoom=[1, conf['mirr_rout'],
+                                                 conf['mirr_rout']],
+                                           elements=[optics.PerfectLens,
+                                                     optics.RadialMirrorScatter,
+                                                     optics.EnergyFilter,
+                                                     optics.EnergyFilter],
+                                           keywords=kwords)
+        self.set_display()
+
+    def set_display(self):
+        self.elements[1].display = {'color': (0.0, 0.5, 0.0), 'opacity': 0.1}
 
 # Gratings
-grating_coords = Table.read(os.path.join(redsoxbase, 'GratingCoordinates.txt'),
-                            format='ascii.commented_header', header_start=2)
-for n in ['pitch', 'yaw', 'roll']:
-    grating_coords[n] = np.deg2rad(grating_coords[n])
-grating_pos = [[p['X'], p['Y'], p['Z']] for p in grating_coords]
-grating_orient = [np.dot(euler2mat(p['roll'], p['pitch'], 0, 'rxyz'), xyz2zxy[:3, :3]) for p in grating_coords]
-grating_zoom = [0.25, 4, 5]
-
-ralfdata = os.path.join(inputpath, 'CATefficiencies_v3.xlsx')
-gratquality = RalfQualityFactor(d=200.e-3, sigma=1.75e-3)
-
-order_selector = InterpolateRalfTable(ralfdata)
-
-# Define L1, L2 blockage as simple filters due to geometric area
-# L1 support: blocks 18 %
-# L2 support: blocks 19 %
-catsupport = optics.GlobalEnergyFilter(filterfunc=lambda e: 0.81 * 0.82)
-
-blazeang = 0.8
-blazemat = transforms3d.axangles.axangle2mat(np.array([0, 0, 1]), np.deg2rad(-blazeang))
-grat_args = {'elem_class': optics.CATGrating,
-             'elem_args': {'d': 2e-4, 'order_selector': order_selector},
-             'id_col': 'grating_id'}
-grat_args_full = copy.deepcopy(grat_args)
-grat_args_full['elem_args']['zoom'] = grating_zoom
-#grat_args_full['elem_args']['orientation'] = np.dot(xyz2zxy[:3, :3], blazemat)
-#grat_args_full['elem_args']['orientation'] = blazemat
-grat_args_full['elem_args']['orientation'] = np.dot(transforms3d.axangles.axangle2mat([1,0,0], np.pi/2), blazemat)
-
-grat1 = GratingGrid(id_num_offset=1000, zoom_grating=grating_zoom[1:],
-                    **grat_args_full)
-grat_pos2 = [np.dot(rotchan2, e.pos4d) for e in grat1.elements]
-grat_pos3 = [np.dot(rotchan3, e.pos4d) for e in grat1.elements]
-grat2 = simulator.Parallel(elem_pos=grat_pos2, id_num_offset=2000, **grat_args)
-grat3 = simulator.Parallel(elem_pos=grat_pos3, id_num_offset=3000, **grat_args)
+def read_grating_coords():
+    '''Read grating coordinates from a file. This is currently not used.'''
+    grating_coords = Table.read(os.path.join(redsoxbase, 'GratingCoordinates.txt'),
+                                format='ascii.commented_header', header_start=2)
+    for n in ['pitch', 'yaw', 'roll']:
+        grating_coords[n] = np.deg2rad(grating_coords[n])
+    trans = [[p['X'], p['Y'], p['Z']] for p in grating_coords]
+    rot = [np.dot(euler2mat(p['roll'], p['pitch'], 0, 'rxyz'),
+                  xyz2zxy[:3, :3]) for p in grating_coords]
+    return trans, rot
 
 
-class CATSupportbars(marxs.base.SimulationSequenceElement):
-    '''carbon fiber structure that holds grating facets will absorb all photons
-    that do not pass through a grating facet.
+class CATGratings(simulator.Sequence):
+    gratquality_class = RalfQualityFactor
 
-    We might want to call this L3 support ;-)
-    '''
-    def process_photons(self, photons):
-        photons['probability'][photons['grating_id'] < 0] = 0.
-        return photons
+    def __init__(self, channels, conf, **kwargs):
+
+        elements = []
+
+        self.gratquality = self.gratquality_class()
+        for chan in channels:
+            elements.append(GratingGrid(channel=chan, conf=conf))
+        elements.extend([catsupport, catsupportbars, self.gratquality])
+        super(CATGratings, self).__init__(elements=elements, **kwargs)
 
 
-grat = simulator.Sequence(elements=[grat1, grat2, grat3, catsupport,
-                                    gratquality, CATSupportbars()])
+class MLMirrors(simulator.Parallel):
+    elem_class = LGMLMirror
+
+    def __init__(self, channels, conf):
+        lgmlpos = [transforms3d.affines.compose(conf['MLpos'],
+                                                np.dot(euler2mat(-np.pi / 4, 0, 0, 'sxyz'),
+                                                       xyz2zxy[:3, :3]),
+                                                conf['MLzoom'])]
+        for chan in '23':
+            lgmlpos.append(np.dot(conf['rotchan'][chan], lgmlpos[0]))
+
+        datafile = os.path.join(inputpath, conf['mirrorfile'])
+        super(MLMirrors, self).__init__(elem_class=self.elem_class,
+                                        elem_pos=lgmlpos,
+                                        elem_args={'datafile': datafile},
+                                        id_num_offset=1,
+                                        id_col='LGML')
 
 
-# ML mirrors
-# mirrorfile = 'ml_refl_2015_minimal.txt'
-mirrorfile = 'ml_refl_2017_withCrSc_width.txt'
+class Detectors(simulator.Sequence):
+    elem_class = optics.FlatDetector
+
+    def filterqe(self):
+        ccdqe = Table.read(os.path.join(inputpath, 'xgs_bi_ccdqe.dat'),
+                           format='ascii.no_header', comment='!',
+                           names=['energy', 'qe' , 'filtertrans', 'temp'])
+        ccdqe['energy'] *= 1e-3  # ev to keV
+        return ccdqe
+
+    def __init__(self, channels, conf):
+        ccdqe = self.ccedq()
+
+        detkwargs = {'pixsize': conf['pixsize']}
+        detzoom0 = np.array([1, pixsize * conf['detsize0'][0] / 2,
+                             pixsize * conf['detsize0'][1] / 2])
+        detzoom = np.array([1, pixsize * conf['detsize123'][0] / 2,
+                            pixsize * conf['detsize123'][1] / 2])
+        # rotate by 45 deg
+        rot = transforms3d.euler.euler2mat(np.pi/4, 0, 0, 'sxyz')
+        # flip to x-z plane
+        flip = np.array([[0, 1, 0], [1, 0, 0], [0, 0, -1]])
+        detposlist = [transforms3d.affines.compose(np.zeros(3), xyz2zxy[:3, :3],
+                                                   detzoom0),
+                      transforms3d.affines.compose([44.55, 25, 0],
+                                                   np.dot(flip, rot),
+                                                   detzoom)]
+        for chan in '23':
+            detposlist.append(np.dot(conf['rotchan'][chan], detposlist[0]))
+
+
+            ccd123_args = {'elements': [self.elem_class, optics.EnergyFilter],
+                           'keywords': [detkwargs,
+                                        {'filterfunc': interp1d(ccdqe['energy'],
+                                                                ccdqe['qe']),
+                                         'name': 'CCD QE'},
+                                    ],
+                       }
+            ccd0_args = copy.deepcopy(ccd123_args)
+            ccd0_args['keywords'][0]['id_num'] = 0
+            ccd0_args['keywords'][0]['id_col'] = 'CCD_ID'
+            # add optical blocking filter for CCD 0
+            ccd0_args['elements'].append(optics.EnergyFilter)
+            ccd0_args['keywords'].append({'filterfunc': interp1d(ccdqe['energy'],
+                                                                 ccdqe['filtertrans']),
+                                          'name': 'optical blocking filter'})
+            det0 = optics.FlatStack(pos4d=detposlist[0], **ccd0_args)
+            det123 = simulator.Parallel(elem_class=optics.FlatStack,
+                                        elem_pos=detposlist[1:],
+                                        elem_args=ccd123_args,
+                                        id_num_offset=1,
+                                        id_col='CCD_ID')
+
+        super(Detectors, self).__init__(elements=[det0, det123])
+        self.set_display()
+
+    def set_display(self):
+        self.elements[0].display = copy.deepcopy(self.elements[0].display)
+        self.elements[0].display['color'] = (0., 0., 1.)
+        for e in self.elements[1].elements:
+            e.display = copy.deepcopy(e.display)
+            e.display['color'] = (0., 0., 1.)
+
+
+class FocalPlaneDet(marxs.optics.FlatDetector):
+    loc_coos_name = ['detfp_x', 'detfp_y']
+    detpix_name = ['detfppix_x', 'detfppix_y']
+
+    def __init__(self, **kwargs):
+        if ('zoom' not in kwargs) and ('pos4d' not in kwargs):
+            kwargs['zoom'] = [.2, 500, 500]
+        if ('orientation' not in kwargs) and ('pos4d' not in kwargs):
+            kwargs['orientation'] = xyz2zxy[:3, :3]
+        if ('position' not in kwargs) and ('pos4d' not in kwargs):
+            kwargs['position'] = [0, 0, -100]
+        super(FocalPlaneDet, self).__init__(**kwargs)
+
+
 LGMLMirror.display['color'] = (1., 0., 1.)
-ml1 = LGMLMirror(datafile=os.path.join(inputpath, mirrorfile),
-                 zoom=[0.25, 15., 5.], position=[44.55, 0, 0],
-                 orientation=np.dot(euler2mat(-np.pi / 4, 0, 0, 'sxyz'),
-                                    xyz2zxy[:3, :3])
-)
-ml2 = LGMLMirror(datafile=os.path.join(inputpath, mirrorfile),
-                 pos4d=np.dot(rotchan2, ml1.pos4d))
-ml3 = LGMLMirror(datafile=os.path.join(inputpath, mirrorfile),
-                 pos4d=np.dot(rotchan3, ml1.pos4d))
-ml = simulator.Sequence(elements=[ml1, ml2, ml3])
-
-# Detectors
-# read optical blocking filter and CCD QE
-ccdqe = Table.read(os.path.join(inputpath, 'xgs_bi_ccdqe.dat'),
-                   format='ascii.no_header', comment='!',
-                   names=['energy', 'qe' , 'filtertrans', 'temp'])
-
-pixsize = 0.016
-detkwargs = {'pixsize': pixsize}
-detzoom0 = np.array([1, pixsize * 408 / 2, pixsize * 1608 / 2])
-detzoom = np.array([1, pixsize * 1632 / 2, pixsize * 1608 / 2])
-# rotate by 45 deg
-rot = transforms3d.euler.euler2mat(np.pi/4, 0, 0, 'sxyz')
-# flip to x-z plane
-flip = np.array([[0, 1, 0], [1, 0, 0], [0, 0, -1]])
-detposlist = [transforms3d.affines.compose(np.zeros(3), xyz2zxy[:3, :3],
-                                           detzoom0),
-              transforms3d.affines.compose([44.55, 25, 0],
-                                           np.dot(flip, rot),
-                                           detzoom)]
-detposlist.append(np.dot(rotchan2, detposlist[1]))
-detposlist.append(np.dot(rotchan3, detposlist[1]))
-det = simulator.Parallel(elem_class=optics.FlatDetector,
-                         elem_pos=detposlist,
-                         elem_args=detkwargs)
-
-ccd123_args = {'elements': [optics.FlatDetector, optics.EnergyFilter],
-               'keywords': [detkwargs,
-                            {'filterfunc': interp1d(ccdqe['energy'] * 1e-3,
-                                                    ccdqe['qe']),
-                             'name': 'CCD QE'},
-                           ],
-               }
-ccd0_args = copy.deepcopy(ccd123_args)
-ccd0_args['keywords'][0]['id_num'] = 0
-ccd0_args['keywords'][0]['id_col'] = 'CCD_ID'
-ccd0_args['elements'].append(optics.EnergyFilter)
-ccd0_args['keywords'].append({'filterfunc': interp1d(ccdqe['energy'] * 1e-3,
-                                                     ccdqe['filtertrans']),
-                              'name': 'optical blocking filter'})
-det0 = optics.FlatStack(pos4d=detposlist[0], **ccd0_args)
-det123 = simulator.Parallel(elem_class=optics.FlatStack,
-                            elem_pos=detposlist[1:],
-                            elem_args=ccd123_args,
-                            id_num_offset=1,
-                            id_col='CCD_ID')
-det = simulator.Sequence(elements=[det0, det123])
-det0.display = copy.deepcopy(det0.display)
-det0.display['color'] = (0., 0., 1.)
-for e in det123.elements:
-    e.display = copy.deepcopy(e.display)
-    e.display['color'] = (0., 0., 1.)
-
-# Place an additional detector in the focal plane for comparison
-# Detectors are transparent to allow this stuff
-detfp = optics.FlatDetector(position=[0, 0, -100], orientation=xyz2zxy[:3, :3],
-                            zoom=[.2, 500, 500])
-detfp.loc_coos_name = ['detfp_x', 'detfp_y']
-detfp.detpix_name = ['detfppix_x', 'detfppix_y']
-detfp.display['opacity'] = 0.1
 
 
-keeppos = simulator.KeepCol('pos')
-redsox = simulator.Sequence(elements=[aper, mirror, grat, ml, det],
-                            postprocess_steps=[keeppos])
+class PerfectRedsox(simulator.Sequence):
 
-redsoxfp = simulator.Sequence(elements=[aper, mirror, grat, ml, det, detfp],
-                              postprocess_steps=[keeppos])
+    aper_class = CircleAperture
+    mirr_class = SimpleMirror
+    cat_class = CATGratings
+    ml_class = MLMirrors
 
-chan1 = simulator.Sequence(elements=[aper, mirror, grat1, ml1, det.elements[1]])
+    def add_detectors(self, conf):
+        return [Detectors]
+
+    def post_process(self):
+        self.KeepPos = simulator.KeepCol('pos')
+        return [self.KeepPos]
+
+    def __init__(self, channels=['1', '2', '3'], conf=conf,
+                 **kwargs):
+        list_of_classes = [self.aper_class, self.mirr_class,
+                           self.cat_class, self.ml_class]
+        elem = []
+        for c in list_of_classes:
+            if c is not None:
+                elem.append(c(channels, conf))
+        elem.extend(self.add_detectors(conf))
+
+        super(PerfectRedsox, self).__init__(elements=elem,
+                                            postprocess_steps=self.post_process(),
+                                            **kwargs)
